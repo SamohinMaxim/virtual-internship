fbd3bfc18b42900f4db359de77ae64b37508a359
SamokhinMaxim <Samohin721@yandex.ru>
2026-05-15 00:34:01 +0500

    создана БД virtual_internship

---
+++ schema.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict VbFedpu516iyIW7D4PPIb6A7P3hWz4PhtcSVi5sgdzEFGcehQ6qf2JbwqtYg7al
+
+-- Dumped from database version 18.4
+-- Dumped by pg_dump version 18.4
+
+-- Started on 2026-05-15 00:33:25
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2026-05-15 00:33:25
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict VbFedpu516iyIW7D4PPIb6A7P3hWz4PhtcSVi5sgdzEFGcehQ6qf2JbwqtYg7al
+
